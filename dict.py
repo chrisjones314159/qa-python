5912b2fb5ba604253791f57d7953090ddaeadbf2
chrisJ <chrisleonjones@gmail.com>
2023-06-26 16:14:12 +0100

    hello world, dicts and user input practise

---
+++ dict.py
@@ -0,0 +1,5 @@
+books = {"authorA": ["book1", "book2"], "authorB": ["book3", "book4"], "authorC": ["book5", "book6"]}
+# author = input("Author's name: ")
+# print(", ".join(books[author]))
+
+print(", ".join(books[input("Author's name: ")]))
---
+++ hello_world.py
@@ -0,0 +1,16 @@
+# Chris' repo https://github.com/chris-reeves1/pyhon-mod2-26-06-23
+
+# print("Hello world")
+
+# username = "chris"
+# age = 27
+# print(f"{username} is {age} years old.")
+
+# username = input("Please enter your name: ")
+# age = int(input("Please enter your age: "))
+# print(f"{username} is {age} years old.")
+
+length = int(input("What is the length of the rectangle: "))
+width = int(input("What is the width of the rectangle: "))
+print(f"The area is {length*width}")
+print(f"The perimeter is {length*2+width*2}")
