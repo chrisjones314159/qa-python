0310ad33c220700d2ac48fe3a5cc220e845107fc
chrisJ <chrisleonjones@gmail.com>
2023-06-27 12:35:30 +0100

    adding square root fnction for task 4

--- conditionals.py
+++ conditionals.py
@@ -1,90 +1,91 @@
-mark = int(input("Grade: "))
+import math
+# mark = int(input("Grade: "))
 
-if mark > 85:
-    print("Distinction")
-elif mark > 65:
-    print("Pass")
-else:
-    print("Fail")
+# if mark > 85:
+#     print("Distinction")
+# elif mark > 65:
+#     print("Pass")
+# else:
+#     print("Fail")
 
-weight = float(input("Weight: "))
-unit = input("Unit (kg or lbs): ").lower()
+# weight = float(input("Weight: "))
+# unit = input("Unit (kg or lbs): ").lower()
 
-if unit == "kg":
-    print(weight*2.205)
-else:
-    print(weight/2.205)
+# if unit == "kg":
+#     print(weight*2.205)
+# else:
+#     print(weight/2.205)
 
-# Chap 2 lab part 1
-age = 15
-if age >= 18:
-    print("Category A")
-elif age >=16:
-    print("Category B")
-else:
-    print("Category C")
+# # Chap 2 lab part 1
+# age = 15
+# if age >= 18:
+#     print("Category A")
+# elif age >=16:
+#     print("Category B")
+# else:
+#     print("Category C")
 
-# Part 2
-num1 = int(input("First number: "))
-num2 = int(input("Second number: "))
+# # Part 2
+# num1 = int(input("First number: "))
+# num2 = int(input("Second number: "))
 
-operation = input("Which operation (+ * / -): ")
+# operation = input("Which operation (+ * / -): ")
 
-if operation == "+":
-    print(num1 + num2)
-elif operation == "-":
-    print(num1 - num2)
-elif operation == "*":
-    print(num1 * num2)
-else:
-    print(num1 / num2)
+# if operation == "+":
+#     print(num1 + num2)
+# elif operation == "-":
+#     print(num1 - num2)
+# elif operation == "*":
+#     print(num1 * num2)
+# else:
+#     print(num1 / num2)
 
-# TASK 2
-grade = int(input("Grade: "))
+# # TASK 2
+# grade = int(input("Grade: "))
 
-if grade > 100 or grade < 0:
-    print("Unsuitable value")
-elif grade >= 71:
-    print("Distinction")
-elif grade >= 61:
-    print("Merit")
-elif grade >= 50:
-    print("Pass")
-elif grade < 50:
-    print("Fail")
-else:
-    print("Something bad happened")
+# if grade > 100 or grade < 0:
+#     print("Unsuitable value")
+# elif grade >= 71:
+#     print("Distinction")
+# elif grade >= 61:
+#     print("Merit")
+# elif grade >= 50:
+#     print("Pass")
+# elif grade < 50:
+#     print("Fail")
+# else:
+#     print("Something bad happened")
 
-# Task 3
-grade = int(input("Grade: "))
-level = int(input("Level (1 or 2): "))
+# # Task 3
+# grade = int(input("Grade: "))
+# level = int(input("Level (1 or 2): "))
 
-if grade > 100 or grade < 0:
-    print("Grade value is not suitable")
-elif level == 1:
-    if grade >= 71:
-        print("Distinction")
-    elif grade >= 61:
-        print("Merit")
-    elif grade >= 50:
-        print("Pass")
-    elif grade < 50:
-        print("Fail")
-    else:
-        print("Something bad happened")
-elif level == 2:
-    if grade >= 66:
-        print("Distinction")
-    elif grade >= 51:
-        print("Merit")
-    elif grade >= 40:
-        print("Pass")
-    elif grade < 40:
-        print("Fail")
-    else:
-        print("Something bad happened")
-else:
-    print("Level Value not suitable")
+# if grade > 100 or grade < 0:
+#     print("Grade value is not suitable")
+# elif level == 1:
+#     if grade >= 71:
+#         print("Distinction")
+#     elif grade >= 61:
+#         print("Merit")
+#     elif grade >= 50:
+#         print("Pass")
+#     elif grade < 50:
+#         print("Fail")
+#     else:
+#         print("Something bad happened")
+# elif level == 2:
+#     if grade >= 66:
+#         print("Distinction")
+#     elif grade >= 51:
+#         print("Merit")
+#     elif grade >= 40:
+#         print("Pass")
+#     elif grade < 40:
+#         print("Fail")
+#     else:
+#         print("Something bad happened")
+# else:
+#     print("Level Value not suitable")
 
 
 # Task 4
@@ -100,14 +101,14 @@ if choice < 1 or choice > 3:
 elif choice == 1:
     c = int(input("C: "))
     b = int(input("B: "))
-    print(f"A = {c**2 + b**2}")
+    print(f"A = {math.sqrt(c**2 + b**2)}")
 elif choice == 2:
     a = int(input("A: "))
     c = int(input("C: "))
-    print(f"B = {c**2 + a**2}")
+    print(f"B = {math.sqrt(c**2 + a**2)}")
 elif choice == 3:
     a = int(input("A: "))
     b = int(input("B: "))
-    print(f"C = {a**2 + b**2}")
+    print(f"C = {math.sqrt(a**2 + b**2)}")
 else:
     print("Something went wrong")
